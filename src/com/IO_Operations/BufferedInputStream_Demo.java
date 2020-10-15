1901a7c325d10a5566424c8aa26c4d4bd3cee65b
Amit Kumar <amitnitsri25@gmail.com>
2020-10-15 23:25:57 +0530

    UC5 : Using BufferedInputStream

--- src/com/IO_Operations/BufferedInputStream_Demo.java
+++ src/com/IO_Operations/BufferedInputStream_Demo.java
@@ -2,16 +2,21 @@ package com.IO_Operations;
 
 import java.io.FileInputStream;
 import java.io.*;
+public class BufferedInputStream_Demo {
 
-public class FileInputStream_Demo {
 
 	public static void main(String[] args) {
 		int ch = 0;
-		FileInputStream fis = null;//
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		byte[] buffer = new byte[1024];
+		String str = new String();
 
 		try {
-			 fis = new FileInputStream("D:\\Capg_Personal\\IO Operations\\src\\com\\IO_Operations\\log1.txt");
-			while ((ch = fis.read()) != -1) {
+			 bis = new BufferedInputStream(new FileInputStream("D:\\Capg_Personal\\IO Operations\\src\\com\\IO_Operations\\log1.txt"));
+			while ((ch = bis.read()) != -1) {
+				//str = new String(buffer,0,ch);
+				//System.out.print(str);
 				System.out.print((char) ch);
 			}
 		} catch (FileNotFoundException e) {
@@ -21,7 +26,7 @@ public class FileInputStream_Demo {
 		}
 		finally {
 			try {
-				fis.close();
+				bis.close();
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 
